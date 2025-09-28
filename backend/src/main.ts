83e9a8ac661466da1f551058b8657cccbae4b0a1
Monnier-Stephane <kameamea133@gmail.com>
2025-09-28 18:51:48 +0200

    cors issues fixed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,7 @@ export async function bootstrap() {
     origin: [
       'https://add-management2.vercel.app',
       'https://add-management2-lkuaczyow-monnier-stehanes-projects.vercel.app',
+      'https://add-management2-1wiqxd2x6-monnier-stehanes-projects.vercel.app',
       'http://localhost:3000', // Pour le développement local
       'http://localhost:3001'  // Pour le développement local
     ],
